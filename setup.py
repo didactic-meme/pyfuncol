1a4e48e16b6f1cb5e280f6a8d33861220406c078
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-12-18 19:05:06 +0100

    Add setup.py and setup.cfg for the package

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfuncol",
+    description="Functional collections extension functions for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gondolav/pyfuncol",
+    author="Andrea Veneziano",
+    author_email="andrea.veneziano@icloud.com",
+    maintainer="Andrea Veneziano",
+    maintainer_email="andrea.veneziano@icloud.com",
+    license="MIT",
+    keywords="functional pipeline data collection chain",
+    packages=find_packages(exclude=["contrib", "docs", "tests*", "test"]),
+    version="0.1",
+    install_requires=["forbiddenfruit"],
+    extras_requires={"dev": ["pytest"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
