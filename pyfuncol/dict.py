7f4f95b00ada339ac90977963739bfa6d1556f60
Samuel Chassot <14821693+samuelchassot@users.noreply.github.com>
2021-12-19 15:10:45 +0100

    Add functions to `dict` (#5)
    * add count to dict
    
    * add foldLeft and foldRight to dict
    
    * add forall to dict
    
    * add find to dict
    
    * add filter_not to dict
    
    * docstring
    
    * reformat code + docstring
    
    Co-authored-by: Andrea Veneziano <andrea.veneziano@icloud.com>

--- pyfuncol/dict.py
+++ pyfuncol/dict.py
@@ -1,5 +1,5 @@
 from forbiddenfruit import curse
-from typing import Callable, Dict, Tuple, TypeVar, List
+from typing import Callable, Dict, Optional, Tuple, TypeVar, List
 
 A = TypeVar("A")
 B = TypeVar("B")
@@ -108,6 +108,128 @@ def to_list(self: Dict[A, B]) -> List[Tuple[A, B]]:
     return [(k, v) for k, v in self.items()]
 
 
+def count(self: Dict[A, B], p: Callable[[Tuple[A, B]], bool]) -> int:
+    """
+    Counts the number of elements in the collection which satisfy a predicate.
+
+    Note: will not terminate for infinite-sized collections.
+
+    Args:
+        p: The predicate used to test elements.
+
+    Returns:
+        The number of elements satisfying the predicate p.
+    """
+    c = 0
+    for t in self.items():
+        if p(t):
+            c += 1
+
+    return c
+
+
+def fold_left(self: Dict[A, B], z: B, op: Callable[[B, Tuple[A, B]], B]) -> B:
+    """
+    Applies a binary operator to a start value and all elements of this collection, going left to right.
+
+    Note: will not terminate for infinite-sized collections.
+
+    Note: might return different results for different runs, unless the underlying collection type is ordered or the operator is associative and commutative.
+
+    Args:
+        z: The start value.
+        op: The binary operator.
+
+    Returns:
+        The result of inserting op between consecutive elements of this collection, going left to right with the start value z on the left:
+
+        op(...op(z, x_1), x_2, ..., x_n)
+        where x1, ..., xn are the elements of this collection. Returns z if this collection is empty.
+    """
+    acc = z
+    for t in self.items():
+        acc = op(acc, t)
+
+    return acc
+
+
+def fold_right(self: Dict[A, B], z: B, op: Callable[[Tuple[A, B], B], B]) -> B:
+    """
+    Applies a binary operator to a start value and all elements of this collection, going right to left.
+
+    Note: will not terminate for infinite-sized collections.
+
+    Note: might return different results for different runs, unless the underlying collection type is ordered or the operator is associative and commutative.
+
+    Args:
+        z: The start value.
+        op: The binary operator.
+
+    Returns:
+        The result of inserting op between consecutive elements of this collection, going right to left with the start value z on the right:
+
+        op(x_1, op(x_2, ... op(x_n, z)...))
+        where x1, ..., xn are the elements of this collection. Returns z if this collection is empty.
+    """
+    acc = z
+    for t in reversed(self.items()):
+        acc = op(t, acc)
+
+    return acc
+
+
+def forall(self: Dict[A, B], p: Callable[[Tuple[A, B]], bool]) -> bool:
+    """
+    Tests whether a predicate holds for all elements of this collection.
+
+    Note: may not terminate for infinite-sized collections.
+
+    Args:
+        p: The predicate used to test elements.
+
+    Returns:
+        True if this collection is empty or the given predicate p holds for all elements of this collection, otherwise False.
+    """
+    for t in self.items():
+        if not p(t):
+            return False
+    return True
+
+
+def find(self: Dict[A, B], p: Callable[[Tuple[A, B]], bool]) -> Optional[Tuple[A, B]]:
+    """
+    Finds the first element of the collection satisfying a predicate, if any.
+
+    Note: may not terminate for infinite-sized collections.
+
+    Note: might return different results for different runs, unless the underlying collection type is ordered.
+
+    Args:
+        p: The predicate used to test elements.
+
+    Returns:
+        An option value containing the first element in the collection that satisfies p, or None if none exists.
+    """
+    for t in self.items():
+        if p(t):
+            return t
+
+    return None
+
+
+def filter_not(self: Dict[A, B], p: Callable[[Tuple[A, B]], bool]) -> Dict[A, B]:
+    """
+    Selects all elements of this iterable collection which do not satisfy a predicate.
+
+    Args:
+        p: The predicate to satisfy.
+
+    Returns:
+        The filtered dict.
+    """
+    return {k: v for k, v in self.items() if not p((k, v))}
+
+
 def extend_dict():
     """
     Extends the dict built-in type with methods.
@@ -120,3 +242,9 @@ def extend_dict():
     curse(dict, "is_empty", is_empty)
     curse(dict, "map", map)
     curse(dict, "to_list", to_list)
+    curse(dict, "count", count)
+    curse(dict, "fold_left", fold_left)
+    curse(dict, "fold_right", fold_right)
+    curse(dict, "forall", forall)
+    curse(dict, "find", find)
+    curse(dict, "filter_not", filter_not)
--- pyfuncol/tests/test_dict.py
+++ pyfuncol/tests/test_dict.py
@@ -37,3 +37,35 @@ def test_map():
 
 def test_to_list():
     assert d.to_list() == [("a", 1), ("b", 2), ("c", 3)]
+
+
+def test_count():
+    assert d.count(lambda kv: (kv[0] == "a" or kv[0] == "b") and kv[1] <= 3) == 2
+
+
+def test_fold_left():
+    assert d.fold_left("", lambda acc, kv: acc + kv[0] + str(kv[1])) == "a1b2c3"
+
+
+def test_fold_right():
+    assert d.fold_right("", lambda kv, acc: acc + kv[0] + str(kv[1])) == "c3b2a1"
+
+
+def test_forall():
+    assert d.forall(lambda kv: kv[1] <= 3) == True
+
+
+def test_forall_false():
+    assert d.forall(lambda kv: kv[1] < 2) == False
+
+
+def test_find():
+    assert d.find(lambda kv: kv[1] == 2) == ("b", 2)
+
+
+def test_find_none():
+    assert d.find(lambda kv: kv[1] == 5) == None
+
+
+def test_filter_not():
+    assert d.filter_not(lambda kv: kv[1] > 1) == {"a": 1}
