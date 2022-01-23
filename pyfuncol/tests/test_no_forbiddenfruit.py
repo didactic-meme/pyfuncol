c5820b599b8333efcaa8e30530b6abe8738e96f5
m-doescode <80221594+m-doescode@users.noreply.github.com>
2022-01-23 19:43:15 +0100

    Added test for dict and set for completeness

--- pyfuncol/tests/test_no_forbiddenfruit.py
+++ pyfuncol/tests/test_no_forbiddenfruit.py
@@ -1,7 +1,13 @@
-from ..list import *
+from .. import dict as pfcdict
+from .. import list as pfclist
+from .. import set as pfcset
 import pytest
 
-my_list = [1, 2, 3]
+def test_dict():
+    assert pfcdict.map({'a': 1, 'b': 2, 'c': 3}, lambda x: (x[0], x[1] * 2)) == {'a': 2, 'b': 4, 'c': 6}
 
-def test_map():
-    assert map(my_list, lambda x: x * 2) == [2, 4, 6]
+def test_list():
+    assert pfclist.map([1, 2, 3], lambda x: x * 2) == [2, 4, 6]
+
+def test_set():
+    assert pfcset.map({1, 2, 3}, lambda x: x * 2) == {2, 4, 6}
